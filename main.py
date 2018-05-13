c9a101e66c81a8d07de27c559a60712bbac4ec78
Advaith Anand <advaith@mit.edu>
2018-05-13 17:34:43 -0400

    Added randomization for preference lists

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from rush import Student, Frat, Rush
-
+import random 
 # s1 = Student("S1", [])
 # s2 = Student("S2", [])
 # s3 = Student("S3", [])
@@ -15,31 +15,37 @@ from rush import Student, Frat, Rush
 # strategies = {f1:"top", f2:"top"}
 # students = [s1, s2, s3]
 
-num_students = 3
-num_frats = 2
-num_swaps=2
-capacity = 2
-strategy = "gale"
+NUM_STUDENTS = 3 #this should be changed 
+NUM_FRATS = 2 #this can also be changed
+NUM_SWAPS=2 #this should be changed 
+CAPACITY = 2 #min_capacity is 1, max_capacity is number of students 
+STRATEGY = "gale"
 
-frats = []
-students = []
-for i in range(num_students):
-	students.append(Student("S%d"%i, []))
-for i in range(num_frats):
-	frats.append(Frat("F%d"%i, capacity, students))
-for s in students:
-	s.acceptable = frats
-strategies = {}
-for f in frats:
-	strategies[f] = strategy
+def generate_rush_params():
+	frats = []
+	students = []
+	list_of_students = []
+	list_of_frats = []
+	for i in range(NUM_STUDENTS):
+		students.append(Student("S%d"%i, []))
+	for i in range(NUM_FRATS):
+		frats.append(Frat("F%d"%i, CAPACITY, random.sample(students, len(students))))
+	for s in students:
+		s.acceptable = random.sample(frats, len(frats)) #randomize the student's preferences
+	strategies = {}
+	for f in frats:
+		strategies[f] = STRATEGY
+	return frats, students, strategies
 
+def main():
+	frats, students, strategies = generate_rush_params()
+	rush = Rush(frats, students, num_swaps=NUM_SWAPS, strategies=strategies)
+	print "BEFORE RUSH"
+	print rush
+	rush.apply_swaps()
+	rush.bid_and_pledge()
+	print "\nAFTER RUSH"
+	print rush
 
-rush = Rush(frats, students, num_swaps=num_swaps, strategies=strategies)
-print "BEFORE RUSH"
-print rush
-
-rush.apply_swaps()
-rush.bid_and_pledge()
-
-print "\nAFTER RUSH"
-print rush
+if __name__ == "__main__":
+	main()
