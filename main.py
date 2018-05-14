952ec5c2ba39fd419d61fe16163cb618f7185d23
Advaith Anand <advaith@mit.edu>
2018-05-14 00:30:26 -0400

    Re-did preferences so they are based on divisions

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 from rush import Student, Frat, Rush
 import random 
 import numpy as np
+import sys
 
-NUM_STUDENTS = 300 
+NUM_STUDENTS = 400 
 NUM_FRATS = 25
-NUM_SWAPS=20 
+NUM_SWAPS= 10 
 STRATEGY = "gale"
-CAPACITY_MEAN = 12 #11.88 for real
-CAPACITY_VARIANCE = 2 
+CAPACITY_MEAN = 11.88 #11.88 for real
+CAPACITY_VARIANCE = 5 
+FRAT_DIVISIONS = 7
+STUDENT_DIVISIONS = 40
+
+#TODO - add latent parmaeter for type of student 
 
 def generate_rush_params():
 	frats = []
 	students = []
-	list_of_students = []
-	list_of_frats = []
 	for i in range(NUM_STUDENTS):
 		students.append(Student("S%d"%i, []))
+	student_split = np.array_split(np.asarray(students), STUDENT_DIVISIONS)
 	for i in range(NUM_FRATS):
 		random_capacity = np.rint(np.random.normal(CAPACITY_MEAN, CAPACITY_VARIANCE))
-		random_acceptable = random.sample(students, len(students))
+		random_acceptable = []
+		for s in student_split:
+			random_acceptable.extend(random.sample(s, len(s)))
+		# random_acceptable = random.sample(students, len(students)) #completely random
 		frats.append(Frat("F%d"%i, random_capacity, random_acceptable))
+	frat_split = np.array_split(np.asarray(frats), FRAT_DIVISIONS)
 	for s in students:
-		s.acceptable = random.sample(frats, len(frats)) #randomize the student's preferences
+		random_acceptable = []
+		for f in frat_split:
+			random_acceptable.extend(random.sample(f, len(f)))
+		s.acceptable = random_acceptable
+		# s.acceptable = random.sample(frats, len(frats)) #completely random
 	strategies = {}
 	for f in frats:
 		strategies[f] = STRATEGY
@@ -42,7 +54,10 @@ def main():
 
 def generate_metrics(rush, no_swap_outcomes):
 	print("----------------\nMETRICS\n----------------\n")
+	total_num_pledges = 0
 	for f in rush.frats:
+		num_pledges = len(f.pledges)
+		total_num_pledges += len(f.pledges)
 		ranks = []
 		no_swap_ranks = []
 		pledge_overlap = 0
@@ -52,13 +67,16 @@ def generate_metrics(rush, no_swap_outcomes):
 				pledge_overlap += 1
 		for p in no_swap_outcomes[f]:
 			no_swap_ranks.append(f.acceptable.index(p) + 1)
-		print("Fraternity: " + f.name)
-		print("Number of Pledges: " + str(len(f.pledges)))
-		print("Number of Pledges without Swaps: " + str(len(no_swap_outcomes[f])))
-		print("Pledge Overlap: " + str(pledge_overlap))
-		print("Pledge Overlap Pct.: " + str(100 * pledge_overlap / len(f.pledges)) + "%")
-		print("Average No Swap Pledge Ranking: " + str(np.mean(no_swap_ranks)))
-		print("Average Pledge Ranking: " + str(np.mean(ranks)))
+		print("------------------------Fraternity: " + f.name + "------------------------")
+		print("Capacity: " + str(f.capacity))
+		print("Number of Students: " + str(num_pledges))
+		print("Capacity Filled: " + str(100 * num_pledges/f.capacity if f.capacity else 0) + "%")
+		print("Number of Students without Swaps: " + str(len(no_swap_outcomes[f])))
+		print("Student Overlap: " + str(pledge_overlap))
+		print("Student Overlap Pct.: " + str(100 * pledge_overlap / num_pledges if num_pledges else 0) + "%")
+		print("Average No Swap Ranking of Matched Students: " + str(np.mean(no_swap_ranks)))
+		print("Average Ranking of Matched Students: " + str(np.mean(ranks)))
+	print("Total Number of Students Matched: " + str(total_num_pledges))
 
 if __name__ == "__main__":
 	main()
